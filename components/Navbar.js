a7878baba9262154889f21107e6e9e9f901077fb
Matthew Ng <matthewtng.2@gmail.com>
2022-04-13 22:18:49 +0800

    add: mobile navbar

--- components/Navbar.js
+++ components/Navbar.js
@@ -8,12 +8,26 @@ import {
   IconButton,
   useBreakpointValue,
   useColorMode,
-  useColorModeValue as mode
+  useDisclosure,
+  Drawer,
+  DrawerOverlay,
+  DrawerContent,
+  DrawerHeader,
+  DrawerBody,
+  Stack,
+  useColorModeValue as mode,
 } from '@chakra-ui/react'
 import * as React from 'react'
-import { FiMenu, FiMoon, FiSun, FiMail } from 'react-icons/fi'
+import { useState } from 'react'
+import { FiMenu, FiMoon, FiSun, FiMail, FiCloud, FiFeather, FiBookOpen } from 'react-icons/fi'
 import { Logo } from './Logo'
 import Link from 'next/link'
+
+const icons = {
+  'about': <FiFeather />,
+  'approach': <FiCloud />,
+  'service': <FiBookOpen />
+}
 
 function ToggleTheme() {
   const { toggleColorMode } = useColorMode()
@@ -30,10 +44,13 @@ function ToggleTheme() {
 }
 
 export const Navbar = () => {
+  const [tab, setTab] = useState(null)
+
   const isDesktop = useBreakpointValue({
     base: false,
     lg: true,
   })
+  const { isOpen, onOpen, onClose } = useDisclosure()
   return (
     <Box
       as="section"
@@ -82,8 +99,46 @@ export const Navbar = () => {
                 <IconButton
                   variant="ghost"
                   icon={<FiMenu fontSize="1.25rem" />}
+                  onClick={ onOpen }
                   aria-label="Open Menu"
                 />
+                <Drawer placement='left' size='xs' isOpen={isOpen} finalFocusRef={tab} onClose={onClose}>
+                  <DrawerOverlay />
+                  <DrawerContent>
+                    <DrawerHeader borderBottomWidth='1px'>
+                      <Logo />
+                    </DrawerHeader>
+                    <DrawerBody >
+                      <Stack spacing="4" align='flex-start'>
+                          {['About', 'Service', 'Approach'].map((item, idx) => (
+                            <Link 
+                              href={`/#${item.toLowerCase()}`}
+                              passHref
+                              key={idx}
+                            >
+                              <Button
+                                onClick={() => {
+                                  setTab(item.toLowerCase())
+                                  onClose()
+                                }}
+                                leftIcon={icons[item.toLowerCase()]}
+                                variant="ghost"
+                              >
+                                {item}
+                              </Button>
+                            </Link>
+                          ))}
+                        <a target="_blank" href="mailto:matthewtng.2@gmail.com"
+                        rel="noopener noreferrer"
+                        >
+                          <Button variant="ghost" borderRadius="5" onClick={onClose} leftIcon={<FiMail />}>
+                            Contact
+                          </Button>
+                        </a>
+                      </Stack>                      
+                    </DrawerBody>
+                  </DrawerContent>
+                </Drawer>
                 <ToggleTheme />
               </HStack>
             )}
